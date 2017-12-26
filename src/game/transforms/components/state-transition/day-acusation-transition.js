7b85643c5fb94a1d6b1a3d09e24a0e194b504cba
Adam Kalman <dkeka.zkfaks@gmail.com>
2017-12-26 14:14:05 -0800

    fix bug in acusation logic
    where it counted dead players to determine threshold for going to
    trial

--- src/game/transforms/components/state-transition/day-acusation-transition.js
+++ src/game/transforms/components/state-transition/day-acusation-transition.js
@@ -1,28 +1,32 @@
-import { DAY_CONDEMNATION, NIGHT } from '../../../game-states'
+import { DAY_CONDEMNATION, NIGHT } from '../../../game-states';
+import { ALIVE } from '../../../character-states';
 
 export default () => {
-   return (state) => {
-     let playerMap = state.players.reduce(
-       (acc, curr) => Object.assign(acc, { [curr]: 0 }),
-       {});
+  return (state) => {
+    let playerMap = state.players.reduce(
+      (acc, curr) => Object.assign(acc, { [curr]: 0 }),
+      {});
 
-     Object.values(state.acusations).forEach(acusation => {
-       playerMap[acusation] = playerMap[acusation] + 1;
-     });
+    Object.values(state.acusations).forEach(acusation => {
+      playerMap[acusation] = playerMap[acusation] + 1;
+    });
 
-     let mostVoted = Object.keys(playerMap).reduce(
-       (acc, curr) => playerMap[curr] > playerMap[acc] ? curr : acc);
+    let mostVoted = Object.keys(playerMap)
+      .reduce((acc, curr) => playerMap[curr] > playerMap[acc] ? curr : acc);
 
+    const alivePlayerCount = state.players
+      .filter(player => state.playerStates[player].characterState === ALIVE)
+      .length;
 
-     return (playerMap[mostVoted] > state.players.length / 2) ?
-       Object.assign({ }, state, {
-         state: DAY_CONDEMNATION,
-         acusations: { },
-         acused: mostVoted
-       }) :
-       Object.assign({ }, state, {
-         state: NIGHT,
-         acusations: { }
-       });
-   };
+    return (playerMap[mostVoted] > alivePlayerCount / 2) ?
+      Object.assign({ }, state, {
+        state: DAY_CONDEMNATION,
+        acusations: { },
+        acused: mostVoted
+      }) :
+      Object.assign({ }, state, {
+        state: NIGHT,
+        acusations: { }
+      });
+  };
 };

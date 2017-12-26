02793c8dded727d58f0328ba85b4af0307e914f7
Adam Kalman <dkeka.zkfaks@gmail.com>
2017-12-26 13:49:10 -0800

    add in base logic for processing votes
    logic for killing still missing

--- src/game/transforms/components/state-transition/day-condemnation-transition.js
+++ src/game/transforms/components/state-transition/day-condemnation-transition.js
@@ -1,9 +1,26 @@
-import { NIGHT } from '../../../game-states'
+import { NIGHT } from '../../../game-states';
+import votes from '../../../vote-options';
 
 export default () => {
-   return (state) => {
-     return Object.assign({ }, state, {
-       state: NIGHT
-     });
-   };
+  return (state) => {
+    let newState = Object.assign({ }, state, {
+      state: NIGHT,
+      votes: { },
+      acused: ''
+    });
+
+    const guiltyVotes = Object.values(state.votes)
+      .filter(vote => vote === votes.GUILTY)
+      .length;
+    const innocentVotes = Object.values(state.votes)
+      .filter(vote => vote === votes.INNOCENT)
+      .length;
+
+    if (guiltyVotes > innocentVotes) {
+      //TODO: kill player and check for game end
+      console.log('killing player goes here')
+    }
+
+    return newState;
+  };
 };
